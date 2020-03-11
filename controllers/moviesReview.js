a819e9e6a1863af7a6f1dcea46d5cdebc8bf678f
Ricardo José Gómez <rjgdevapp@gmail.com>
2020-03-11 06:55:09 -0500

    modifies the save route in moviesReviewRouter to handle updating and error catching of movies reviews

--- controllers/moviesReview.js
+++ controllers/moviesReview.js
@@ -3,25 +3,34 @@ const moviesReviewRouter = require('express').Router()
 const config = require('../utils/config')
 const Review = require('../models/Review')
 
-moviesReviewRouter.get('/:movieid', async(req, res, next) => {
+moviesReviewRouter.get('/:movieid', async (req, res, next) => {
 	const movie_id = req.params.movieid
 	const reviews = await Review.find({ movie_id: movie_id })
 	res.status(200).json(reviews)
 })
 
-moviesReviewRouter.post('/:movieid/save', async(req, res, next) => {
+moviesReviewRouter.post('/:movieid/save', async (req, res, next) => {
 
 	const movie_id = req.params.movieid
 	const body = req.body
 
-	const review = new Review({
-		email: body.email,
-		movie_id: movie_id,
-		rating: body.rating,
-		comment: body.comment
-	})
-
 	try {
+
+		let review = new Review()
+
+		if (body.id) {
+			review = await Review.findById(body.id)
+			if (!review) {
+				return res.status(400).json({ error: 'Id not found' })
+			}
+		} else {
+			review.email = body.email
+			review.movie_id = movie_id
+		}
+
+		review.rating = body.rating
+		review.comment = body.comment
+
 		const savedReview = await review.save()
 		res.status(200).json(savedReview)
 	} catch (err)  {
--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,7 +1,7 @@
 const errorHandler = (err, request, res, next) => {
   
 	if (err.name === 'ValidationError') {
-		return res.status(400).json({ err: err.message })
+		return res.status(400).json({ error: err.message })
 	}
   
 	next(err)
